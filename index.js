7c2cf5fd717338e262712754fa2ab4c751357aff
Frano Stanisic <fsta657@aucklanduni.ac.nz>
2018-09-09 06:23:36 +1200

    Add intro text

--- index.js
+++ index.js
@@ -9,15 +9,33 @@ const sw = Splitwise({
   logger: console.log
 });
 
-sw.getCurrentUser().then((info) => {
-  console.log("It's working?");
-  console.log(info);
-});
+function getHowToText () {
+  let msg = 'Before we get started, you\'re going to have to add me to your splitwise group.\n';
+  msg += 'My email is `toichi@tfwno.gf`, please don\'t sign me up for newsletters, thanks. ';
+  msg += 'Once you\'ve done that, you need to let me know what the group id is for this server (I manage a lot of clients). ';
+  msg += 'You can do this by navigating to the group and grabbing the numbers at the end. For example, ';
+  msg += 'if the page you end up on is https://secure.splitwise.com/#/groups/1234567, your id is `1234567`. Capiche?\n';
+  msg += 'Once you\'ve got that, type ' + bot.user.toString() + '` set <your group id>`, and assuming I can find it you\'re all set.\n';
+  return msg;
+}
 
 bot.login(process.env.TOKEN || credentials.discordToken);
 
 bot.on('ready', function (event) {
   console.log('Logged in as %s - %s\n', bot.user.username, bot.user.id);
+});
+
+bot.on('guildCreate', function (guild) {
+  // Just added to guild, look for channel to post intro in
+  const channelRef = guild.channels.find(channel => channel.type === 'text' && channel.permissionsFor(guild.me).has('SEND_MESSAGES'));
+  if (!channelRef) return; // Nowhere to send to, oh well
+  let msg = 'Hey, thanks for having me! I\'m Toichi, pleasure to make your acquaintance.\n';
+  msg += 'You know, they all told me *"Toichi, bulls can\'t be accountants!"*. But despite that, ';
+  msg += 'here I am, working for the prestigious, uh, *' + guild.name + '* server. Living the dream eh? Anyway... \n';
+  msg += getHowToText();
+  msg += 'Alright, all sorted? If you ever get stuck, just type ' + bot.user.toString() + '` help`, ';
+  msg += 'and I\'ll do my best to assist you.';
+  channelRef.send(msg);
 });
 
 bot.on('message', function (message) {
